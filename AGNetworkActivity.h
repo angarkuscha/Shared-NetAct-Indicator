dcce6918dd31811b8c7d940664b42217dfd7d4d7
angarkuscha <andreas.garkuscha@gmx.de>
2013-06-18 14:11:08 +0200

    Class added

---
+++ AGNetworkActivity.h
@@ -0,0 +1,14 @@
+//
+//  AGNetworkActivity.h
+//
+//  Created by Andreas Garkuscha on 6/18/13.
+//  Copyright (c) 2013 Andreas Garkuscha. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGNetworkActivity : NSObject
+
++ (AGNetworkActivity*)sharedNetworkActivity;
+
+@end
---
+++ AGNetworkActivity.m
@@ -0,0 +1,31 @@
+//
+//  AGNetworkActivity.m
+//
+//  Created by Andreas Garkuscha on 6/18/13.
+//  Copyright (c) 2013 Andreas Garkuscha. All rights reserved.
+//
+
+#import "AGNetworkActivity.h"
+
+static __weak AGNetworkActivity* sharedInstance = nil;
+
+@implementation AGNetworkActivity
+
++ (AGNetworkActivity*)sharedNetworkActivity {
+    AGNetworkActivity* currentActivity;
+    @synchronized([AGNetworkActivity class]) {
+        if (!sharedInstance) {
+            sharedInstance = currentActivity = [[AGNetworkActivity alloc] init];
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+        } else {
+            currentActivity = sharedInstance;
+        }
+    }
+    return currentActivity;
+}
+
+- (void)dealloc {
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+}
+
+@end
